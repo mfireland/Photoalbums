efc3236c93e5936c18dc77ebf44a3fe5c7461925
Mike Ireland <mike.ireland@elsevier.com>
2015-04-23 14:38:27 -0500

    Add photo search feature; reformat code
    Add photo search feature per book.
    Generally reformat code to suit my taste better.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Photoalbums' });
+    res.render('index', { title: 'Photoalbums' });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -68,7 +68,7 @@ var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
     var connection  = mysql.createConnection(globals.database());
     connection.connect(function(err) {
-        if (err){
+        if (err) {
             console.log('error connecting to database:', err);
         } else {
             console.log('connected to database!');
