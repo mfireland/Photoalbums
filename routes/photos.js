6d6d5c936035499bcb44778e1bb6f08a9cc7788b
Mike Ireland <mike.ireland@elsevier.com>
2015-04-24 15:19:59 -0500

    Add support for uploading photos to an S3 bucket
    Add aws-sdk and multer modules.  Add code to call aws-sdk to upload
    photo files to an S3 bucket and addthe URL in the DB.

--- routes/photos.js
+++ routes/photos.js
@@ -1,65 +1,105 @@
 var express = require('express');
 var router = express.Router();
 var model = require('./../lib/model/model-photos');
+var globals = require('./../lib/globals');
+var fs = require('fs');
 
 /* GET photo by ID */
 router.get('/id/:id', function(req, res) {
-    if (req.param('id')) {
-	var params = {
-	    photoID : req.param('id')
-	}
-	model.getPhotoByID(params, function(err, obj) {
-	    if (err) {
-		res.status(400).send({error: 'Invalid photo ID'});
-	    } else {
-		res.send(obj);
-	    }
-	});
-    } else {
-	res.status(400).send({error: 'Invalid login'});		
+  if (req.param('id')) {
+    var params = {
+      photoID : req.param('id')
     }
+    model.getPhotoByID(params, function(err, obj) {
+      if (err) {
+	res.status(400).send({error: 'Invalid photo ID'});
+      } else {
+	res.send(obj);
+      }
+    });
+  } else {
+    res.status(400).send({error: 'Invalid login'});		
+  }
 });
 
 /* GET photo search */
 router.get('/search', function(req, res) {
-    res.header('Cache-Control', 'no-cache, no-store');
-    if (req.param('query')) {
-        var params = {
-	    query : req.param('query')
-        }
-        model.getPhotosSearch(params, function(err, obj) {
-	    if (err) {
-	      res.status(400).send({error: 'Invalid photo search'});
-	    } else {
-	      res.send(obj);
-	    }
-        });
-    } else {
-        res.status(400).send({error: 'No search term found'});
+  res.header('Cache-Control', 'no-cache, no-store');
+  if (req.param('query')) {
+    var params = {
+      query : req.param('query')
     }
+    model.getPhotosSearch(params, function(err, obj) {
+      if (err) {
+	res.status(400).send({error: 'Invalid photo search'});
+      } else {
+	res.send(obj);
+      }
+    });
+  } else {
+    res.status(400).send({error: 'No search term found'});
+  }
 });
 
 /* POST create photo. */
 router.post('/upload', function(req, res) {
-    if (req.param('albumID') && req.param('userID')) {
-	var params = {
-	    userID : req.param('userID'),
-	    albumID : req.param('albumID')
-	}
-	if (req.param('caption')) {
-	    params.caption = req.param('caption');
-	}
-
-	model.createPhoto(params, function(err, obj) {
-	    if (err) {
-		res.status(400).send({error: 'Invalid photo data'});
-	    } else {
-		res.send(obj);
-	    }
-	});
-    } else {
-	res.status(400).send({error: 'Invalid photo data'});
+  if (req.param('albumID') && req.param('userID')) {
+    var params = {
+      userID : req.param('userID'),
+      albumID : req.param('albumID')
     }
+    if (req.param('caption')) {
+      params.caption = req.param('caption');
+    }
+
+    model.createPhoto(params, function(err, obj) {
+      if (err) {
+	res.status(400).send({error: 'Invalid photo data'});
+      } else {
+	res.send(obj);
+      }
+    });
+  } else {
+    res.status(400).send({error: 'Invalid photo data'});
+  }
+});
+router.post('/upload', function(req, res) {
+  if (req.param('albumID') && req.param('userID') && req.files.photo) {
+    var params = {
+      userID : req.param('userID'),
+      albumID : req.param('albumID')
+    }
+    if (req.param('caption')) {
+      params.caption = req.param('caption');
+    }
+    fs.exists(req.files.photo.path, function(exists) {
+      if (exists) {
+        params.filePath = req.files.photo.path;
+        var timestamp = Date.now();
+        params.newFilename = params.userID + '/' + params.filePath.replace('tmp/', timestamp);
+        uploadPhoto(params, function(err, fileObject) {
+          if (err) {
+            res.status(400).send({error: 'Invalid photo data'});
+          } else {
+            params.url = fileObject.url;
+            delete params.filePath;
+            delete params.newFilename;
+            model.createPhoto(params, function(err, obj) {
+              if (err) {
+                res.status(400).send({error: 'Invalid photo data'});
+              } else {
+                res.send(obj);
+              }
+	    });
+	  }
+	});
+      } else {
+	res.status(400).send({error: 'Invalid photo data'});
+      }
+    });
+  } else {
+    res.status(400).send({error: 'Invalid photo data'});
+  }
 });
 
 /* POST delete photo. */
@@ -79,6 +119,53 @@ router.post('/delete', function(req, res) {
 	res.status(400).send({error: 'Invalid photo ID'});
     }
 });
+
+// Private functions
+function uploadPhoto(params, callback) {
+  fs.readFile(params.filePath, function (err, imgData) {
+    if (err) {
+      callback(err);
+    } else {
+      var contentType = 'image/jpeg';
+      var uploadPath = 'uploads/' + params.newFilename;
+      var uploadData = {
+        Bucket : globals.awsVariables().bucket,
+        Key : uploadPath,
+        Body : imgData,
+        ACL :'public-read',
+        ContentType : contentType
+      }
+      putS3Object(uploadData, function(err, data) {
+        if (err) {
+          callback(err);
+        } else {
+          fs.unlink(params.filePath, function (err) {
+            if (err) {
+              callback(err);
+            } else {
+              callback(null, {url: uploadPath});
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+function putS3Object(uploadData, callback) {
+  var aws = require('aws-sdk');
+  if (globals.awsVariables().key) {
+    aws.config.update({ accessKeyId: globals.awsVariables().key, secretAccessKey: globals.awsVariables().secret });
+  }
+  var s3 = new aws.S3();
+  s3.putObject(uploadData, function(err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
+  });
+}
 
 
 module.exports = router;
