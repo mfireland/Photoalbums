8d8ec353c8ecd352475e9b9794be8482c4d02333
Mike Ireland <mike.ireland@elsevier.com>
2015-04-28 14:30:22 -0500

    Fix new environment port reference.

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.set('port', globals.applicationPort());
+app.set('port', globals.applicationPort().port);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
