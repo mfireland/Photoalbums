87f676df2f170b3b3454670f6885ad72f8d058c7
Mike Ireland <mike.ireland@elsevier.com>
2015-04-28 14:56:21 -0500

    Simplify port config code.

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.set('port', globals.applicationPort().port);
+app.set('port', globals.applicationPort());
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
