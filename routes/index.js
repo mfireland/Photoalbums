62aaff281f963dbb40af10680e992032f35674be
Recursive Lambda God <chris@circle.academy>
2017-04-29 13:49:40 -0400

    "Recursive Lambda Refactor V1"

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var debug = require('debug')('photoalbums:router:index');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
     debug('/: title="%s"', 'Photoalbums');
     res.render('index', { title: 'Photoalbums' });
 });
